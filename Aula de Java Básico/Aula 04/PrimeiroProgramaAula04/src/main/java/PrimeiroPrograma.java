07d8bb9f010456c1ecfbc7e444210576b16cbd02
OsniFilipo <osnifilipo@gmail.com>
2022-04-17 21:37:37 -0300

    Update PrimeiroPrograma.java

--- Aula de Java Básico/Aula 04/PrimeiroProgramaAula04/src/main/java/PrimeiroPrograma.java
+++ Aula de Java Básico/Aula 04/PrimeiroProgramaAula04/src/main/java/PrimeiroPrograma.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package primeiroprograma;
 
-/**
- *
- * @author Pichau
- */
 public class PrimeiroPrograma {
-    
+
+    public static void main(String[] args){
+
+        System.out.print("Olá, Mundo!");
+    }
 }
